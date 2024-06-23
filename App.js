0152b800110b0ee35000fcb7537ac2892530bb73
SKRPTZ <dosomethingwild012712@gmail.com>
2024-06-23 01:22:02 -0600

    z

--- App.js
+++ App.js
@@ -1,36 +1,53 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./screens/HomeScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const Stacks = ()=>{
-    const Stack = createStackNavigator()
-    return(
+import HomeScreen from "./screens/HomeScreen";
+import MatchScreen from "./screens/MatchScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+const Stacks = () => {
+    const Stack = createStackNavigator();
+    return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen}/>
-        </Stack.Navigator>/
-    )
-}
+            <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+    );
+};
 
 export default function App() {
-    
-    const Tab = createBottomTabNavigator()
-    
+    const Tab = createBottomTabNavigator();
+
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Home Page" component={HomeScreen}/>
+                <Tab.Screen
+                    name="Home Page"
+                    component={Stacks}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: () => <Ionicons name="home" />,
+                    }}
+                />
+                <Tab.Screen
+                    name="Matches"
+                    component={MatchScreen}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: () => <Ionicons name="home" />,
+                    }}
+                />{" "}
+                <Tab.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: () => <Ionicons name="home" />,
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,68 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
+import { useState } from "react";
 
 export default function HomeScreen() {
+    const [steamId, setSteamId] = useState("");
+
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.mainContainer}>
+            <View style={styles.searchView}>
+                <TextInput
+                    style={styles.idInput}
+                    onChangeText={setSteamId}
+                    value={steamId}
+                    placeholder="Steam ID"
+                    keyboardType="numeric"
+                />
+                <Pressable
+                    style={styles.searchButton}
+                    onPress={() => {
+                        //Validating Id Size before fetch in next tab//
+                        if (steamId.length === 17) {
+                            console.log(`ID#${steamId} is ready for search`);
+                        } else {
+                            console.error(
+                                `Invalid ID length (${steamId.length}/17 Digits)`
+                            );
+                        }
+                    }}
+                >
+                    <Text>Search</Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        height: "100%",
+    },
+    searchView: {
+        marginTop: 5,
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    idInput: {
+        height: 40,
+        flex: 5,
+        backgroundColor: "white",
+        margin: 10,
+        fontSize: 20,
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        borderWidth: 2,
+        borderRadius: 150,
+    },
+    searchButton: {
+        height: 40,
+        flex: 1,
+        backgroundColor: "white",
+        margin: 10,
+        paddingHorizontal: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        borderWidth: 2,
+        borderRadius: 50,
+    },
+});
